a0854ce994b49dc4e445342b97b2b114f5bd3845
adimerpaul <adimer101@gmail.com>
2021-11-11 14:17:08 -0400

    mitad proyecto

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -14,10 +14,16 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
     <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA==" crossorigin=""></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<!--    <link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.0/css/font-awesome.css" rel="stylesheet">-->
+<!---->
+<!--    <link rel="stylesheet" href="--><?//=base_url()?><!--dist/leaflet.awesome-markers.css">-->
+<!--    <script src="--><?//=base_url()?><!--dist/leaflet.awesome-markers.js"></script>-->
 
-    <link rel="stylesheet" href="<?=base_url()?>dist/leaflet.awesome-markers.css">
-    <script src="<?=base_url()?>dist/leaflet.awesome-markers.js"></script>
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/leaflet.extra-markers.css">
+    <script src="<?=base_url()?>assets/js/leaflet.extra-markers.js"></script>
 
     <!---->
 	<style>
@@ -33,7 +39,7 @@
             position: absolute;
             top: 0px;
             left: 0px;
-            padding: 10px;
+            /*padding: 10px;*/
             margin-top: 10px;
             margin-left: 50px;
             z-index: 400;
@@ -42,9 +48,9 @@
             position: absolute;
             top: 0px;
             left: 0px;
-            padding: 10px;
+            /*padding: 10px;*/
             margin-top: 10px;
-            margin-left: 130px;
+            margin-left: 120px;
             z-index: 400;
         }
         #titulo {
@@ -52,8 +58,8 @@
             top: 0px;
             left: 0px;
             padding: 10px;
-            margin-top: 10px;
-            margin-left: 310px;
+            margin-top: 5px;
+            margin-left: 260px;
             z-index: 400;
         }
         #logooruro {
@@ -62,7 +68,7 @@
             left: 0px;
             padding: 10px;
             margin-top: 0px;
-            margin-left: 200px;
+            margin-left: 230px;
             z-index: 400;
         }
         #logouto {
@@ -81,8 +87,8 @@
 <body>
 
 <div id='map'></div>
-<input type="button" onclick='location.href="<?=base_url()?>Welcome"' id="btnlugares" class="btn btn-primary" value="Lugares" />
-<input type="button" onclick='location.href="<?=base_url()?>Welcome/recorridos"' id="recorridos" class="btn btn-primary" value="Recorridos" />
+<input type="button" onclick='location.href="<?=base_url()?>"' id="btnlugares" class="btn btn-success btn-sm" value="Postes" />
+<input type="button" onclick='location.href="<?=base_url()?>Welcome/recorridos"' id="recorridos" class="btn btn-danger btn-sm" value="Mantenimiento" />
 <img src="<?=base_url()?>img/oruro.png" id="logooruro" alt="" width="50">
 <!--<img src="--><?//=base_url()?><!--img/uto.png" id="logouto" alt="" width="50">-->
 <label for="" style="font-weight: bold;color: #002166" id="titulo">Geo Referencia-ORURO</label>
@@ -130,20 +136,20 @@
         var overlays = {
             "Lugares": lugares
         };
-        var LeafIcon = L.Icon.extend({
-            options: {
-                iconSize:     [38, 95],
-                shadowSize:   [50, 64],
-                iconAnchor:   [22, 94],
-                shadowAnchor: [4, 62],
-                popupAnchor:  [-3, -76]
-            }
-        });
-        L.control.layers(baseLayers, overlays).addTo(map);
-        var greenIcon = new LeafIcon({
-            iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
-            shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png'
-        })
+        // var LeafIcon = L.Icon.extend({
+        //     options: {
+        //         iconSize:     [38, 95],
+        //         shadowSize:   [50, 64],
+        //         iconAnchor:   [22, 94],
+        //         shadowAnchor: [4, 62],
+        //         popupAnchor:  [-3, -76]
+        //     }
+        // });
+        // L.control.layers(baseLayers, overlays).addTo(map);
+        // var greenIcon = new LeafIcon({
+        //     iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
+        //     shadowUrl: 'http://leafletjs.com/examples/custom-icons/leaf-shadow.png'
+        // })
 
         datos();
         function datos(){
@@ -155,24 +161,39 @@
                     // console.log(dat);
                     dat.forEach(r=>{
                         // console.log(r);
-                        var redMarker = L.AwesomeMarkers.icon({
-                            // icon: 'cog',
-                            // prefix: 'fa',
-                            markerColor: r.color,
-                            spin:true
-                        });
+                        // var redMarker = L.AwesomeMarkers.icon({
+                        //     // icon: 'cog',
+                        //     // prefix: 'fa',
+                        //     markerColor: r.color,
+                        //     spin:true
+                        // });
 
-                        L.marker([r.lat, r.lng], {icon: redMarker}).bindPopup(r.codigo+' <span class="eliminar" data-id="'+r.id+'"><i class="fa fa-trash-alt"></i></span>').addTo(lugares);
+                        var redMarker = L.ExtraMarkers.icon({
+                            icon: 'fa-number',
+                            number: r.poste,
+                            // innerHTML:'<p style="color: white">'+r.poste+'</p>',
+                            markerColor:r.color,
+                            // svg:true,
+                            // svgBorderColor:'#fff'
+                        });
+                        var html='<table border="1">' +
+                            '<tr><td><b>Codigo:</b></td><td>'+r.codigo+'</td></tr>' +
+                            '<tr><td><b>Nro de post:</b></td><td>'+r.poste+'</td></tr>' +
+                            '<tr><td><b>Tipo:</b></td><td>'+r.tipo+'</td></tr>' +
+                            '<tr><td><b>Potencia:</b></td><td>'+r.potencia+' W</td></tr>' +
+                            '<tr><td colspan="2"><b><button class="btn btn-danger btn-sm">Mantenimiento</button></b></td></tr>' +
+                            '</table>'
+                        L.marker([r.lat, r.lng], {icon: redMarker}).bindPopup(html+' <!--span class="eliminar" data-id="'+r.id+'"><i class="fa fa-trash-alt"></i></span-->').addTo(lugares);
                     })
                 }
             });
         }
         var popup = L.popup();
         function onMapClick(e) {
-            popup
-                .setLatLng(e.latlng)
-                .setContent("<input placeholder='codigo' id='codigo' /><br><input placeholder='potencia' id='potencia' /><br><input placeholder='tipo luminaria' id='tipo' /><br><input placeholder='poste' id='poste' /><button class='crear' id-lat='"+e.latlng.lat+"' id-lng='"+e.latlng.lng+"'>Crear </button>" + e.latlng.toString())
-                .openOn(map);
+            // popup
+            //     .setLatLng(e.latlng)
+            //     .setContent("<input placeholder='codigo' id='codigo' /><br><input placeholder='potencia' id='potencia' /><br><input placeholder='tipo luminaria' id='tipo' /><br><input placeholder='poste' id='poste' /><button class='crear' id-lat='"+e.latlng.lat+"' id-lng='"+e.latlng.lng+"'>Crear </button>" + e.latlng.toString())
+            //     .openOn(map);
         }
         $('#map').on('click','.crear',function (e){
             // console.log($('#nombre').val());
